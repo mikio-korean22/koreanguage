39f9c1cab13588fd97181aac7b85e7f3a1d99fe8
unknown <mikio.fullmoon.korea@gmail.com>
2020-11-30 14:02:56 +0900

    first_files

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    
+    //カルーセル
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        items: 1,
+        autoplay: true
+    });
+    
+    //メニュー
+    $("#sp_navi_btn").click(function() {
+        $(this).next('ul').slideToggle();
+    });
+    
+    $(".list-materials .list-title").click(function(){
+            $(this).next('.ichiran').slideToggle();
+        });
+    
+    $("#top-btn").click(function() {
+            $("html,body").animate({
+                "scrollTop": 0
+            }, 500);
+        });
+
+        new WOW().init();
+});
